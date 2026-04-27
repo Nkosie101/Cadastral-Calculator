657732456fa5094183bc303d086376b41f605863
Nkosie101 <126993300+Nkosie101@users.noreply.github.com>
2026-04-28 00:12:15 +0200

    Add files via upload

---
+++ Surveyors Calculator/Templates/FlyoutItem.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Surveyors_Calculator.Templates;
+
+public partial class FlyoutItem : ContentView
+{
+    public FlyoutItem()
+    {
+        InitializeComponent();
+    }
+
+
+    public static readonly BindableProperty IconProperty =
+                BindableProperty.Create(nameof(Icon), typeof(string), typeof(FlyoutItem));
+
+    public static readonly BindableProperty TitleProperty =
+                BindableProperty.Create(nameof(Title), typeof(string), typeof(FlyoutItem));
+
+    public string Icon
+    {
+        get => (string)GetValue(IconProperty);
+        set => SetValue(IconProperty, value);
+    }
+    public string Title
+    {
+        get => (string)GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+
+
+
+}
+
