e606c833d6bf56f5c7e7d48126155c0f95b79b44
Nkosie101 <126993300+Nkosie101@users.noreply.github.com>
2026-04-27 23:35:00 +0200

    Add files via upload

---
+++ GlobalUsing.cs
@@ -0,0 +1,17 @@
+﻿global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using Surveyors_Calculator.Model;
+global using Surveyors_Calculator.ViewModel;
+global using System.Collections.ObjectModel;
+global using System.ComponentModel;
+global using System.Diagnostics;
+global using System.Runtime.CompilerServices;
+global using System.Text.Json;
+global using SQLite;
+global using SQLitePCL;
+global using System.Threading.Tasks;
+global using CSharpShellCore;
+global using Microsoft.Maui.ApplicationModel;
+global using CommunityToolkit.Maui.Views;
+global using Surveyors_Calculator.Popups;
+
