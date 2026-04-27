95c34f1402940b8d021324ffe53e6d3bb5cfa222
Nkosie101 <126993300+Nkosie101@users.noreply.github.com>
2026-04-27 23:25:30 +0200

    Add files via upload

---
+++ View/Settings.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace Surveyors_Calculator.View
+{
+    public partial class Settings : ContentPage
+    {
+        public Settings(SettingsViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+
+        }
+    }
+}
