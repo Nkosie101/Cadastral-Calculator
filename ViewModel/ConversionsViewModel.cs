26241c04667e7ac9c79fd7569967ee45fef4742b
Nkosie101 <126993300+Nkosie101@users.noreply.github.com>
2026-04-27 23:30:55 +0200

    Add files via upload

---
+++ ViewModel/ConversionsViewModel.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+//using CommunityToolkit.Mvvm.ComponentModel;
+//using System.Threading.Tasks;
+using Surveyors_Calculator.View;
+//using Surveyors_Calculator.Model;
+
+
+namespace Surveyors_Calculator.ViewModel;
+
+
+public partial class ConversionsViewModel : BaseViewModel
+{
+    public ObservableCollection<string> ConversionUnitsFrom { get; } = new();
+    public ObservableCollection<string> ConversionUnitsTo { get; } = new();
+    public ObservableCollection<string> ConversionTypes { get; } = new();
+    public ObservableCollection<string> UnitSystems { get; } = new();
+
+    public ConversionsViewModel()
+    {
+        //formulae = new Formulae { name = "Join", image = "join_image.avif" };
+        //Formulas.Add(formulae);
+        ConversionTypes = new ObservableCollection<string>();
+        ConversionUnitsFrom = new ObservableCollection<string>();
+        ConversionUnitsTo = new ObservableCollection<string>();
+        UnitSystems = new ObservableCollection<string>();
+        //GetConversion();
+
+        UnitSystems.Add("Metric");
+        UnitSystems.Add("Cape/English");
+
+        ConversionTypes.Add("Length");
+        ConversionTypes.Add("Area");
+
+    }
+
+
+
+
+    [ObservableProperty]
+    private double input = 0;
+    [ObservableProperty]
+    private string output = string.Empty;
+    [ObservableProperty]
+    private string selectedUnit1 = string.Empty;
+    [ObservableProperty]
+    private string selectedUnit2 = string.Empty;
+    [ObservableProperty]
+    private string selectedType = string.Empty;
+    [ObservableProperty]
+    private string unitSystem = "Metric";
+    [ObservableProperty]
+    private double factor = 0;
+
+    partial void OnSelectedTypeChanged(string value)
+    {
+        ConversionUnitsFrom.Clear();
+        ConversionUnitsTo.Clear();
+        SelectedUnit1 = string.Empty;
+        SelectedUnit2 = string.Empty;
+        FillUnitsCommand.Execute(null);
+
+    }
+
+    partial void OnUnitSystemChanged(string value)
+    {
+        ConversionUnitsFrom.Clear();
+        ConversionUnitsTo.Clear();
+        SelectedUnit1 = string.Empty;
+        SelectedUnit2 = string.Empty;
+        FillUnitsCommand.Execute(null);
+    }
+
+    [RelayCommand]
+    async Task FillUnits()
+    {
+        if (SelectedType == "Area")
+        {
+            if (UnitSystem == "Metric")
+            {
+                ConversionUnitsFrom.Add("English Sq. feet");
+                ConversionUnitsFrom.Add("Cape Sq. feet");
+                ConversionUnitsFrom.Add("Acres");
+                ConversionUnitsFrom.Add("Cape Sq. roods");
+                ConversionUnitsFrom.Add("Morgen");
+                ConversionUnitsFrom.Add("Sq. roods");
+                ConversionUnitsFrom.Add("Hectares");
+                ConversionUnitsFrom.Add("Sq. Metres");
+
+                ConversionUnitsTo.Add("Sq. Metres");
+                ConversionUnitsTo.Add("Hectares");
+                ConversionUnitsTo.Add("Acres");
+
+
+            }
+            else
+            {
+                ConversionUnitsTo.Add("English Sq. feet");
+                ConversionUnitsTo.Add("Cape Sq. feet");
+                ConversionUnitsTo.Add("Acres");
+                ConversionUnitsTo.Add("Cape Sq. roods");
+                ConversionUnitsTo.Add("Morgen");
+                //ConversionUnitsTo.Add("Sq. roods");
+
+                ConversionUnitsFrom.Add("Sq. Metres");
+                ConversionUnitsFrom.Add("Hectares");
+                ConversionUnitsFrom.Add("Acres");
+            }
+
+
+        }
+        else if (SelectedType == "Length")
+        {
+            //AppShell.Current.DisplayAlert("Error", $"{}", "OK");
+            if (UnitSystem == "Metric")
+            {
+                ConversionUnitsFrom.Add("English feet");
+                ConversionUnitsFrom.Add("Cape feet");
+                ConversionUnitsFrom.Add("Cape roods");
+                ConversionUnitsTo.Add("Metres");
+            }
+            else
+            {
+                ConversionUnitsTo.Add("English feet");
+                ConversionUnitsTo.Add("Cape feet");
+                ConversionUnitsTo.Add("Cape roods");
+                ConversionUnitsFrom.Add("Metres");
+            }
+
+
+        }
+    }
+
+
+
+
+    [RelayCommand]
+    async Task GetConversion()
+    {
+        try
+        {
+            Factor = 0;
+            if (selectedType == "Length" && UnitSystem == "Cape/English")
+            {
+                //AppShell.Current.DisplayAlert("Error", $"{SelectedUnit1}", "OK");
+                switch (SelectedUnit2)
+                {
+                    case "Cape feet":
+                        Factor = 3.17605937;
+                        break;
+                    case "English feet":
+                        Factor = 3.28084558;
+                        break;
+                    case "Cape roods":
+                        Factor = 0.264671614;
+                        break;
+                    default:
+                        Factor = 1;
+                        break;
+                }
+                //AppShell.Current.DisplayAlert("Error", $"{Factor}", "OK");
+            }
+            else if (selectedType == "Length" && UnitSystem == "Metric")
+            {
+                switch (SelectedUnit1)
+                {
+                    case "Cape feet":
+                        Factor = 0.304799472;
+                        break;
+                    case "English feet":
+                        Factor = 0.314855575; break;
+                    case "Cape roods":
+                        Factor = 3.7782669; break;
+                    default:
+                        Factor = 1;
+                        break;
+                }
+            }
+
+            if (selectedType == "Area" && UnitSystem == "Metric")
+            {
+                switch (SelectedUnit1, SelectedUnit2)
+                {
+                    case ("English Sq. feet", "Sq. Metres"):
+                        Factor = 0.092902718;
+                        break;
+                    case ("English Sq. feet", "Hectares"):
+                        Factor = 0.0000092902718;
+                        break;
+                    case ("Acres", "Sq. Metres"):
+                        Factor = 4046.8424;
+                        break;
+                    case ("Acres", "Hectares"):
+                        Factor = 0.40468424;
+                        break;
+                    case ("Cape Sq. feet", "Sq. Metres"):
+                        Factor = 0.0991340332;
+                        break;
+                    case ("Cape Sq. feet", "Hectares"):
+                        Factor = 0.00000991340332;
+                        break;
+                    case ("Cape Sq. roods", "Sq. Metres"):
+                        Factor = 14.2753008;
+                        break;
+                    case ("Cape Sq. roods", "Hectares"):
+                        Factor = 0.00142753008;
+                        break;
+                    case ("Morgen", "Sq. Metres"):
+                        Factor = 8565.18047;
+                        break;
+                    case ("Morgen", "Hectares"):
+                        Factor = 0.856518047;
+                        break;
+                    case ("Morgen", "Acres"):
+                        Factor = 2.116509522;
+                        break;
+                    case ("Sq. roods", "Acres"):
+                        Factor = 0.00352751587;
+                        break;
+                    case ("Hectares", "Sq. Metres"):
+                        Factor = 10000;
+                        break;
+                    case ("Hectares", "Acres"):
+                        Factor = 2.47106238;
+                        break;
+                    case ("Sq. Metres", "Hectares"):
+                        Factor = 0.0001;
+                        break;
+                    case ("Sq. Metres", "Acres"):
+                        Factor = 0.000247106238;
+                        break;
+                    default:
+                        Factor = 1;
+                        break;
+
+                }
+            }
+            else if (selectedType == "Area" && UnitSystem == "Cape/English")
+            {
+                switch (SelectedUnit1, SelectedUnit2)
+                {
+                    case ("Sq. Metres", "English Sq. feet"):
+                        Factor = 10.7639477;
+                        break;
+                    case ("Sq. Metres", "Acres"):
+                        Factor = 0.000247106238;
+                        break;
+                    case ("Hectares", "English Sq. feet"):
+                        Factor = 107639.477;
+                        break;
+                    case ("Hectares", "Acres"):
+                        Factor = 2.47106238;
+                        break;
+                    case ("Sq. Metres", "Cape Sq. feet"):
+                        Factor = 10.0873531;
+                        break;
+                    case ("Sq. Metres", "Cape Sq. roods"):
+                        Factor = 0.0700510634;
+                        break;
+                    case ("Sq. Metres", "Morgen"):
+                        Factor = 0.000116751772;
+                        break;
+                    case ("Hectares", "Cape Sq. feet"):
+                        Factor = 100873.531;
+                        break;
+                    case ("Hectares", "Cape Sq. roods"):
+                        Factor = 700.510634;
+                        break;
+                    case ("Hectares", "Morgen"):
+                        Factor = 1.16751772;
+                        break;
+                    default:
+                        Factor = 1;
+                        break;
+
+                }
+                //AppShell.Current.DisplayAlert("Error", $"{Factor}", "OK");
+
+
+
+            }
+            Output = Convert.ToString(Input * Factor);
+        }
+        catch (Exception e)
+        {
+            await AppShell.Current.DisplayAlert("Error", $"Check your entries! {e.Message}", "OK");
+        }
+        finally
+        {
+
+        }
+    }
+
+}
+
+
+
+
---
+++ ViewModel/QuickTransform3DViewModel.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+/*using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;*/
+using Surveyors_Calculator.View;
+
+namespace Surveyors_Calculator.ViewModel;
+
+public partial class QuickTransform3DViewModel : BaseViewModel
+{
+    public QuickTransform3DViewModel()
+    {
+        FromCoords = new List<Coordinate>();
+        ToCoords = new List<Coordinate>();
+        ToTransform = new List<Coordinate>();
+    }
+
+    Formulae formulae = new Formulae();
+
+    public List<Coordinate> FromCoords { get; } = new();
+    public List<Coordinate> ToCoords { get; } = new();
+    public List<Coordinate> ToTransform { get; } = new();
+
+    [ObservableProperty]
+    private string e1Entry = string.Empty;
+    [ObservableProperty]
+    private string n1Entry = string.Empty;
+    [ObservableProperty]
+    private string z1Entry = string.Empty;
+    [ObservableProperty]
+    private string e2Entry = string.Empty;
+    [ObservableProperty]
+    private string n2Entry = string.Empty;
+    [ObservableProperty]
+    private string z2Entry = string.Empty;
+    [ObservableProperty]
+    private string e3Entry = string.Empty;
+    [ObservableProperty]
+    private string n3Entry = string.Empty;
+    [ObservableProperty]
+    private string z3Entry = string.Empty;
+    [ObservableProperty]
+    private string e4Entry = string.Empty;
+    [ObservableProperty]
+    private string n4Entry = string.Empty;
+    [ObservableProperty]
+    private string z4Entry = string.Empty;
+    [ObservableProperty]
+    private string e5Entry = string.Empty;
+    [ObservableProperty]
+    private string n5Entry = string.Empty;
+    [ObservableProperty]
+    private string z5Entry = string.Empty;
+    [ObservableProperty]
+    private string e6Entry = string.Empty;
+    [ObservableProperty]
+    private string n6Entry = string.Empty;
+    [ObservableProperty]
+    private string z6Entry = string.Empty;
+    [ObservableProperty]
+    private string e7Entry = string.Empty;
+    [ObservableProperty]
+    private string n7Entry = string.Empty;
+    [ObservableProperty]
+    private string z7Entry = string.Empty;
+
+    [ObservableProperty]
+    private string easting = "0";
+    [ObservableProperty]
+    private string northing = "0";
+    [ObservableProperty]
+    private string elevation = "0";
+
+
+
+    [RelayCommand]
+    async Task Get3DTransformedCoordinates()
+    {
+        if (IsBusy)
+            return;
+
+
+        try
+        {
+            IsBusy = true;
+
+            //AppShell.Current.DisplayAlert("Error", $"{ToCoords.Count}", "OK");
+            Coordinate point1 = new Coordinate
+            {
+                name = "p1",
+                x = Convert.ToDouble(e1Entry),
+                y = Convert.ToDouble(n1Entry),
+                z = Convert.ToDouble(z1Entry),
+
+            };
+            ToCoords.Add(point1);
+
+            Coordinate point2 = new Coordinate
+            {
+                name = "p2",
+                x = Convert.ToDouble(e2Entry),
+                y = Convert.ToDouble(n2Entry),
+                z = Convert.ToDouble(z2Entry),
+
+            };
+            ToCoords.Add(point2);
+            Coordinate point3 = new Coordinate
+            {
+                name = "p3",
+                x = Convert.ToDouble(e3Entry),
+                y = Convert.ToDouble(n3Entry),
+                z = Convert.ToDouble(z3Entry),
+
+            };
+            ToCoords.Add(point3);
+            Coordinate point4 = new Coordinate
+            {
+                name = "p4",
+                x = Convert.ToDouble(e4Entry),
+                y = Convert.ToDouble(n4Entry),
+                z = Convert.ToDouble(z4Entry),
+
+            };
+            FromCoords.Add(point4);
+            Coordinate point5 = new Coordinate
+            {
+                name = "p5",
+                x = Convert.ToDouble(e5Entry),
+                y = Convert.ToDouble(n5Entry),
+                z = Convert.ToDouble(z5Entry),
+
+            };
+            FromCoords.Add(point5);
+            Coordinate point6 = new Coordinate
+            {
+                name = "p6",
+                x = Convert.ToDouble(e6Entry),
+                y = Convert.ToDouble(n6Entry),
+                z = Convert.ToDouble(z6Entry),
+
+            };
+            FromCoords.Add(point6);
+
+            Coordinate point7 = new Coordinate
+            {
+                name = "p7",
+                x = Convert.ToDouble(e7Entry),
+                y = Convert.ToDouble(n7Entry),
+                z = Convert.ToDouble(z7Entry),
+
+            };
+            ToTransform.Add(point7);
+
+
+            //var scaleFactor = formulae.GetScaleFactor(e1Entry, n1Entry, e2Entry, n2Entry, e3Entry, n3Entry, e4Entry, n4Entry);
+            //Easting = scaleFactor.ToString();
+            //var rotation = formulae.GetRotation(e1Entry, n1Entry, e2Entry, n2Entry, e3Entry, n3Entry, e4Entry, n4Entry);
+            //formulae.GetTranslation(scaleFactor, rotation, e1Entry, n1Entry, e3Entry, n3Entry);
+            formulae.createInitialMatrices(ToCoords, FromCoords, ToTransform);
+            formulae.SVD();
+            formulae.S3D();
+            formulae.Translation();
+            var result = formulae.Transform3DQuick(E7Entry, N7Entry, Z7Entry);
+            Easting = result.Easting.ToString();
+            Northing = result.Northing.ToString();
+            Elevation = result.Elevation.ToString();
+
+
+            await AppShell.Current.DisplayAlert("Result", $"E: {result.Easting}, N: {result.Northing}, Z: {result.Elevation}", "OK");
+
+
+
+            /*for (int i = 0; i < From.Count; i++)
+            {
+
+                e7Entry = Convert.ToString(From[i].y);
+                n7Entry = Convert.ToString(From[i].x);
+                z7Entry = Convert.ToString(From[i].z);
+
+                var match = To.FirstOrDefault(s => s.name == From[i].name);
+
+                if (match == null)
+                {
+                    var result = formulae.Transform3D(e7Entry, n7Entry, z7Entry);
+                    //AppShell.Current.DisplayAlert("Error", $"{e7Entry}", "OK");
+                    Coordinate toCoord = new Coordinate
+                    {
+                        name = Convert.ToString(From[i].name),
+                        y = Convert.ToDouble(result.Easting),
+                        x = Convert.ToDouble(result.Northing),
+                        z = Convert.ToDouble(result.Elevation),
+                         
+                    };
+                    To.Add(toCoord);
+                }
+            }*/
+        }
+        catch (Exception e)
+        {
+            await AppShell.Current.DisplayAlert("Error", $"Check your entries! {e.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+}
