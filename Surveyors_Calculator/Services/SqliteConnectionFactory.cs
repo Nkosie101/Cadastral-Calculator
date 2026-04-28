f0bfc1419a9ff2dbeeade3cd6a97b823e11f2e1c
Nkosie101 <126993300+Nkosie101@users.noreply.github.com>
2026-04-28 07:10:45 +0200

    Add files via upload

---
+++ Surveyors_Calculator/Services/SqliteConnectionFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+//using SQLite;
+//using System.Threading.Tasks;
+//using SQLite;
+using System.IO;
+using Microsoft.Maui.Storage;
+//using Java.Nio.FileNio;
+
+
+namespace Surveyors_Calculator;
+
+public class SqliteConnectionFactory
+{
+    public SQLiteAsyncConnection CreateConnection()
+    {
+        return new SQLiteAsyncConnection(Path.Combine(FileSystem.AppDataDirectory, "CoordinatesDatabase.db3"), SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.Create | SQLiteOpenFlags.SharedCache, true);
+
+    }
+}
