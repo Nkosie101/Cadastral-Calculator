5273815446842f1cbd5b2a4314c79c1528993050
Nkosie101 <126993300+Nkosie101@users.noreply.github.com>
2026-04-28 07:15:13 +0200

    Add files via upload

---
+++ Surveyors_Calculator/View/About.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace Surveyors_Calculator.View
+{
+    public partial class About : ContentPage
+    {
+        public About(AboutViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+
+
+
+    }
+}
---
+++ Surveyors_Calculator/View/CalculationsMenuPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+//using CommunityToolkit.Mvvm.ComponentModel;
+using Surveyors_Calculator.View;
+
+namespace Surveyors_Calculator.View
+{
+    public partial class CalculationsMenuPage : ContentPage
+    {
+        public CalculationsMenuPage(CalculationsMenuPageViewModel viewModel)
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+    }
+}
