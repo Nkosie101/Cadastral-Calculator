48f5d81d593db67842fea010a6cabf760cbc8cd3
Nkosie101 <126993300+Nkosie101@users.noreply.github.com>
2026-04-28 07:16:08 +0200

    Add files via upload

---
+++ Surveyors_Calculator/ViewModel/MyCSVMapPopupViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Surveyors_Calculator.View;
+
+namespace Surveyors_Calculator.ViewModel;
+
+public partial class MyCSVMapPopupViewModel : BaseViewModel
+{
+
+    public ObservableCollection<string> CoordinateSystems { get; } = new();
+    public ObservableCollection<string> Units { get; } = new();
+
+
+
+    public MyCSVMapPopupViewModel()
+    {
+        Units.Add("metres");
+        Units.Add("Cape feet");
+        CoordinateSystems.Add("Lo 27°");
+        CoordinateSystems.Add("Lo 29°");
+        CoordinateSystems.Add("Lo 31°");
+        CoordinateSystems.Add("Local");
+    }
+
+
+    [ObservableProperty]
+    private string yColumn = string.Empty;
+    [ObservableProperty]
+    private string xColumn = string.Empty;
+    [ObservableProperty]
+    private string zColumn = string.Empty;
+    [ObservableProperty]
+    private string nameColumn;
+    [ObservableProperty]
+    private string missingData;
+
+    // Reference to the View
+    public MyCSVMapPopup Instance { get; set; }
+
+    [RelayCommand]
+    public void OkButton()
+    {
+
+        if (string.IsNullOrEmpty(NameColumn) || string.IsNullOrEmpty(YColumn) || string.IsNullOrEmpty(XColumn) || string.IsNullOrEmpty(ZColumn))
+        {
+            MissingData = "Fill all the fields";
+        }
+        else
+        {
+            /*FileData fileData = new FileData
+            {
+                fileName = FileName,
+                coordSystem = SelectedCoordSys,
+                units = SelectedUnits
+            };*/
+            List<string> columnData = [NameColumn, YColumn, XColumn, ZColumn];
+            Instance?.Close(columnData);
+        }
+
+    }
+
+
+}
---
+++ Surveyors_Calculator/ViewModel/MySavePopupViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using Surveyors_Calculator.View;
+
+namespace Surveyors_Calculator.ViewModel;
+
+public partial class MySavePopupViewModel : BaseViewModel
+{
+
+    public ObservableCollection<string> CoordinateSystems { get; } = new();
+    public ObservableCollection<string> Units { get; } = new();
+
+
+
+    public MySavePopupViewModel()
+    {
+        Units.Add("metres");
+        Units.Add("Cape feet");
+        CoordinateSystems.Add("Lo 27°");
+        CoordinateSystems.Add("Lo 29°");
+        CoordinateSystems.Add("Lo 31°");
+        CoordinateSystems.Add("Local");
+    }
+
+
+    [ObservableProperty]
+    private string selectedCoordSys;
+    [ObservableProperty]
+    private string selectedUnits;
+    [ObservableProperty]
+    private string fileName;
+    [ObservableProperty]
+    private string missingData;
+
+    // Reference to the View
+    public MySavePopup Instance { get; set; }
+
+    [RelayCommand]
+    public void OkButton()
+    {
+
+        if (string.IsNullOrEmpty(FileName) || string.IsNullOrEmpty(SelectedCoordSys) || string.IsNullOrEmpty(SelectedUnits))
+        {
+            MissingData = "Fill all the fields";
+        }
+        else
+        {
+            FileData fileData = new FileData
+            {
+                fileName = FileName,
+                coordSystem = SelectedCoordSys,
+                units = SelectedUnits
+            };
+            Instance?.Close(fileData);
+        }
+
+    }
+
+
+}
