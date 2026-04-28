59ce99b771bfc65620b12fb8d2a82631acabe0ef
Nkosie101 <126993300+Nkosie101@users.noreply.github.com>
2026-04-28 07:11:47 +0200

    Add files via upload

---
+++ Surveyors_Calculator/Templates/FlyoutFooter.xaml.cs
@@ -0,0 +1,11 @@
+﻿//using Microsoft.Maui.Controls;
+
+namespace Surveyors_Calculator.Templates;
+
+public partial class FlyoutFooter : ContentView
+{
+    public FlyoutFooter()
+    {
+        InitializeComponent();
+    }
+}
---
+++ Surveyors_Calculator/Templates/FlyoutHeader.xaml.cs
@@ -0,0 +1,11 @@
+﻿//using Microsoft.Maui.Controls;
+
+namespace Surveyors_Calculator.Templates;
+
+public partial class FlyoutHeader : ContentView
+{
+    public FlyoutHeader()
+    {
+        InitializeComponent();
+    }
+}
